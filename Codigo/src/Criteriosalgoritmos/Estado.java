80ccfd1346193d9a2501059859db7ae584b26044
daniel.mejias <daniel.mejias@estudiantat.upc.edu>
2025-03-14 20:35:54 +0100

    Arreglado problemas en constructor y generador ingenuo de Estado

--- Codigo/src/Criteriosalgoritmos/Estado.java
+++ Codigo/src/Criteriosalgoritmos/Estado.java
@@ -35,8 +35,13 @@ public class Estado {
 
     public Estado(boolean greedy) {
         asignacionSensores = new AsignacionSensor[sensores.size()];
+        ocupacionSensores = new double[sensores.size()];
+        cantidadConexionesSensores = new int[sensores.size()];
         ocupacionCentros = new double[centrosDatos.size()];
         cantidadConexionesCentros = new int[centrosDatos.size()];
+        for (int i = 0; i < ocupacionSensores.length; i++) {
+            ocupacionSensores[i] = sensores.get(i).getCapacidad();
+        }
 
         Arrays.fill(asignacionSensores, -1);
 
@@ -59,11 +64,10 @@ public class Estado {
         for (int i = 0; i < sensores.size(); i++) {
             ConectarA(i,j, false);
             if (ocupacionCentros[j] > 150 || cantidadConexionesCentros[j] > 25) {
-                cantidadConexionesCentros[j]--;
-                ocupacionCentros[j] -= sensores.get(i).getCapacidad();
+                Desconectar(i);
                 j++;
             }
-            asignacionSensores[i].setAssignacion(j);
+            ConectarA(i,j,false);
         }
     }
     //conecta i a j, el booleano indica si j es sensor o centro
